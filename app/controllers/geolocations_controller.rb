3e0c24d6f8388b7b3e037f43cadf7a3713f40c2c
mdidato <f>
2015-07-26 19:36:48 +0100

    fixes google maps url

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -2,6 +2,6 @@ class GeolocationsController < ApplicationController
   def show
       response = HTTParty.get('http://watchbug-api.herokuapp.com/geolocations/100').body
       parsed = JSON.parse(response)
-      redirect_to("http://maps.google.com/maps?q=#{parsed[:lat]},#{parsed[:long]}&z19=")
+      redirect_to("http://maps.google.com/maps?q=#{parsed['lat']},#{parsed['long']}&z19=")
   end
 end
