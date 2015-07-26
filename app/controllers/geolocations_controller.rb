c4116dbf51cea92860c91721b49e7f8a226e7635
mdidato <f>
2015-07-26 18:58:05 +0100

    change heroku app for geolocations

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -1,6 +1,6 @@
 class GeolocationsController < ApplicationController
   def show
-      response = HTTParty.get('http://salty-beyond-5800.herokuapp.com/geolocations/100').body
+      response = HTTParty.get('http://watchbug-api.herokuapp.com/geolocations/100').body
       parsed = JSON.parse(response)
       redirect_to("http://maps.google.com/maps?z=12&t=m&q=loc:#{parsed[:lat]}+#{parsed[:long]}")
   end
