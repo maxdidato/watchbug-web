2382f9ac442d4331108bc81235d4602b3a718565
mdidato <f>
2015-08-01 16:22:05 +0100

    adds battery status and shows map in an iframe

--- app/controllers/geolocations_controller.rb
+++ app/controllers/geolocations_controller.rb
@@ -1,7 +1,9 @@
 class GeolocationsController < ApplicationController
   def show
-      response = HTTParty.get('http://watchbug-api.herokuapp.com/geolocations/100').body
-      parsed = JSON.parse(response)
-      redirect_to("http://maps.google.com/maps?q=#{parsed['lat']},#{parsed['long']}&z19=")
+    response = HTTParty.get('http://localhost:3000/geolocations/100').body
+    parsed = JSON.parse(response)
+    @link ="http://maps.google.com/maps?q=#{parsed['lat']},#{parsed['long']}&z=16&output=embed"
+    @battery = parsed['battery']
+    # redirect_to("http://maps.google.com/maps?q=#{parsed['lat']},#{parsed['long']}&z19=")
   end
 end
