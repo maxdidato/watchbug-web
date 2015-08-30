82824895d73ea48d258c8b0c3f33c194cc6a1e2f
mdidato <f>
2015-08-30 17:22:39 +0100

    adds test coverage for geolocation controller

---
+++ spec/controllers/geolocations_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe GeolocationsController, type: :controller do
+
+  context '#show' do
+
+    context 'when the user is not authenticated' do
+
+      it 'redirects to the sign_in page' do
+        get :show
+        assert_redirected_to new_user_session_path
+      end
+
+    end
+
+    context 'when the user is authenticated' do
+      let(:latitude){rand(999)}
+      let(:longitude){rand(999)}
+      let(:battery){rand(999)}
+      before do
+        sign_in create(:user)
+      end
+
+      it 'gets the geolocation and battery and  set the appropriate instance variables' do
+        allow(subject.current_user).to receive(:geolocation).and_return(lat: latitude,long:longitude,battery:battery)
+        get :show
+        expect(assigns(:link)).to eq("http://maps.google.com/maps?q=#{latitude},#{longitude}&z=16&output=embed")
+        expect(assigns(:battery)).to eq(battery)
+      end
+    end
+  end
+end
